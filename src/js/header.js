9245d5a72e22c59bf948b6f74acf438e8ca480d9
Zheka_Zagrebelnii888 <zyv0007@gmail.com>
2025-09-01 17:20:31 +0200

    create modal window

---
+++ src/js/header.js
@@ -0,0 +1,18 @@
+ document.addEventListener("DOMContentLoaded", () => {
+    const refs = {
+      openBtn: document.querySelector('[data-menu-open]'),
+      closeBtns: document.querySelectorAll('[data-menu-close]'),
+      modal: document.querySelector('[data-menu]')
+    };
+
+    function toggleModal() {
+      refs.modal.classList.toggle('is-open');
+      document.body.classList.toggle('no-scroll');
+    }
+
+    refs.openBtn.addEventListener('click', toggleModal);
+
+    refs.closeBtns.forEach(btn => {
+      btn.addEventListener('click', toggleModal);
+    });
+  });
